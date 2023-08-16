a09ccc708d01f64527a0d013e30704edd17966a4
milk717 <robolindasoo@gmail.com>
2023-08-16 12:49:37 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# BOJ 파일 자동 생성 스크립트
+> BOJ 문제 링크를 입력받아 `문제번호_문제이름_티어` 형식의 파일을 생성해주는 스크립트입니다.  
+# 참고자료
+- [쉘 스크립트 변수명 규칙](https://chanchan-father.tistory.com/802)  
+- [쉘 스크립트 프로그래밍 문법](https://inpa.tistory.com/entry/LINUX-%EC%89%98-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%ED%95%B5%EC%8B%AC-%EB%AC%B8%EB%B2%95-%EC%B4%9D%EC%A0%95%EB%A6%AC)
+- [쉘 스크립트 문법 검사 사이트](https://www.shellcheck.net/)
---
+++ index.js
@@ -0,0 +1,38 @@
+import * as cheerio from 'cheerio';
+import axios from "axios";
+import romanNumerals from "roman-numerals";
+
+const findProblemNumber = (link)=>{
+    return link.split('/').at(-1);
+}
+
+const getSolvedHtml = async (number) => {
+    const link = `https://solved.ac/search?query=${number}`;
+    return await axios.get(link).then(r=>r.data);
+}
+
+const findTierAndName = (html)=>{
+    const $ = cheerio.load(html);
+    const tier = $('img.css-1vnxcg0').attr('alt');
+    const name = $('span.__Latex__').text();
+    return {tier, name};
+}
+
+const tierToShortForm = (tier)=>{
+    return `${tier.at(0)}${romanNumerals.toArabic(tier.at(-1))}`;
+}
+
+const makeFileName = (number, name, tier) => {
+    return [number, name, tier].join('_');
+}
+
+try{
+    const bojLink = process.argv[2];
+    const number = findProblemNumber(bojLink);
+    const html = await getSolvedHtml(number);
+    const {tier, name} = findTierAndName(html);
+    const shortTier = tierToShortForm(tier);
+    console.log(makeFileName(number, name, shortTier))
+}catch (e){
+    process.exit(1);
+}
